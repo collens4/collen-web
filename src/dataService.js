45c8e1326cbdc808c77ff0710de07fbc796d5d97
Ebezer Igbinoba <igbinobaebenezer@gmail.com>
2022-10-25 22:39:06 +0100

    .

---
+++ src/dataService.js
@@ -0,0 +1,19 @@
+export function getProducts() {
+    return getData('/products.json')
+        .then(result => {
+            console.log(result)
+            return result.products
+        });
+}
+
+function getData(endpoint) {
+    const delay = (0.5 + Math.random() * 2) * 1000;
+    return new Promise((resolve, reject) => {
+        setTimeout(function () {
+            fetch(endpoint)
+                .then(res => {
+                    resolve(res.json())
+                });
+        }, delay);
+    });
+}
